4aa7ae58073d72fc1fefd5a9bc17e0582d77d603
Clay Hsiao <claytinghuan@gmail.com>
2020-03-02 16:23:54 +0800

    feat: initialize component todo-list

--- AngularMyTodoList/src/app/todo-list/todo-list.module.ts
+++ AngularMyTodoList/src/app/todo-list/todo-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoListComponent } from './todo-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TodoListComponent],
   imports: [
     CommonModule
   ]
