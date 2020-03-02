2338edda89e5b96999fed8287661967fb47476bb
Clay Hsiao <claytinghuan@gmail.com>
2020-03-02 17:08:36 +0800

    feat: implement addList(title) and getList funciton in the Todolist service

--- AngularMyTodoList/src/app/todo-list/todo-list.service.ts
+++ AngularMyTodoList/src/app/todo-list/todo-list.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class TodoListService {
 
   constructor() { }
+
+  private list: string[] = [];
+
+  /**
+   * Add Todo List
+   */
+  add(title: string): void {
+    if(title || title.trim()) {
+      this.list.push(title);
+    }
+  }
+
+  /**
+   * get the todo list
+   * @returns {string[]}
+   * @memberof TodoListService
+   */
+  getList(): string[] {
+    return this.list;
+  }
 }
