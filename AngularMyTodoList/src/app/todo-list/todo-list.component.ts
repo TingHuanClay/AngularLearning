ab76c685d935f9ebcff50eb9003a7d8b1a99504d
Clay Hsiao <claytinghuan@gmail.com>
2020-03-02 17:33:49 +0800

    feat: bind service and component together and display the result

--- AngularMyTodoList/src/app/todo-list/todo-list.component.ts
+++ AngularMyTodoList/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoListService} from './todo-list.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
+  // previous line is exactly the same as following 3 lines
+
+  // private todoListService: TodoListService;
+  // constructor(private todoListService: TodoListService) {
+  //   this.todoListService = todoListService;
+  // }
+
 
   ngOnInit(): void {
   }
@@ -18,7 +26,22 @@ export class TodoListComponent implements OnInit {
    * @memberof TodoListComponent
    */
   addTodo(inputRef: HTMLInputElement): void {
-    console.log(inputRef.value);
-    inputRef.value = '';
+    // console.log(inputRef.value);
+    // inputRef.value = '';
+
+    const todo = inputRef.value.trim();
+    if (todo) {
+      this.todoListService.add(todo);
+      inputRef.value = '';
+    }
+  }
+
+  /**
+   * get the todo list
+   * @returns {string[]}
+   * @memberof TodoListComponent
+   */
+  getList(): string[] {
+    return this.todoListService.getList();
   }
 }
