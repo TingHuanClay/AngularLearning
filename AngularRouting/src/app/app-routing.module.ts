c14cd24f5cac7b84c49f4243f2128627410000d7
Clay Hsiao <claytinghuan@gmail.com>
2020-03-04 23:18:58 +0800

    feat: initialize routing practice project

---
+++ AngularRouting/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+/**
+ * localhost:4200/#/home  =>  HomeComponent
+ * localhost:4200/#/about =>  AboutComponent
+ */
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
